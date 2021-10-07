9986f299fc9f6b32918544e62b3d46a5db6fb5e4
MilenaZocca <milenaz.skye@gmail.com>
2021-10-06 22:42:48 -0300

    Esboço de Tela de Formula

---
+++ app/src/main/java/com/example/easyapp/MenuActivity.java
@@ -0,0 +1,22 @@
+package com.example.easyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void buttonFormulaClick(View view) {
+        Intent intent = new Intent(this,FormulaActivity.class);
+        startActivity(intent);
+    }
+}
