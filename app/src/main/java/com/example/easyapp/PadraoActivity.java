0755d7472e680f0472b7c9887feeeb826901af01
Rafael <rafaelmartinsfranqueira@gmail.com>
2021-10-20 20:25:18 -0300

    2021-10-20 20:25

--- app/src/main/java/com/example/easyapp/PadraoActivity.java
+++ app/src/main/java/com/example/easyapp/PadraoActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class activity_padrao extends AppCompatActivity {
+public class PadraoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
