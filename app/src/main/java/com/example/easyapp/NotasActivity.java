ac3b0aa4c458b779d065bb8d1f9f66aee2238111
MilenaZocca <milenaz.skye@gmail.com>
2021-10-19 22:33:39 -0300

    Tela Notas

--- app/src/main/java/com/example/easyapp/NotasActivity.java
+++ app/src/main/java/com/example/easyapp/NotasActivity.java
@@ -3,6 +3,8 @@ package com.example.easyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -18,5 +20,15 @@ public class NotasActivity extends AppCompatActivity {
                                     R.array.materias_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
     }
 }
